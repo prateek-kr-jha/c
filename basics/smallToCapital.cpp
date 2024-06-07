d55a08f2edea40169f94f11cbd80d0d7a79953a9
Prateek <prateekj1211@gmail.com>
2024-06-07 22:09:10 +0530

    wrote few basic programs

---
+++ basics/smallToCapital.cpp
@@ -0,0 +1,10 @@
+#include <simplecpp>
+
+main_program {
+    char small, capital;
+    // int diff = ;
+    cout<<"enter an alphabet: ";
+    cin>>small;
+    capital = small + 'A' - 'a';
+    cout <<"capital: "<<capital << "\n";
+}
