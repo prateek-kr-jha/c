3774e0120e14e4a1cd6c5bf0dc850dd53378def0
prateek <prateek.jha@yulu.bike>
2024-09-03 01:33:51 +0530

    added notes

--- io/io.cpp
+++ io/io.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int main() {
-    int x;
+    // int[] x= new int[4];
     int y {};
     // std::cout << "enter number: ";
     // std::cin >> x;
     std::cout << "enter number2: " << sizeof(int);
     std::cin >> y;
 
-    std::cout << "hi there! "<< x << " & " << y << "\n";
+    std::cout << "hi there! " << " & " << y << "\n";
     return 0;
 }
