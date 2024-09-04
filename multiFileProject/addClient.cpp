ea367c04ca647b7939670822db4f8eb018f83c03
prateek <prateek.jha@yulu.bike>
2024-09-04 23:57:20 +0530

    did something

--- multiFileProject/addClient.cpp
+++ multiFileProject/addClient.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#define TEXT "enter first number: "
+ #define ADD
+
 int add(int num1, int num2);
 int main() {
     int num1{};
     int num2{};
-    std::cout << "enter first number: ";
+    std::cout << TEXT;
+    #ifdef ADD
     std::cin >> num1;
     std::cout << "enter second number: ";
     std::cin >> num2;
     std::cout << "sum is " << add(num1, num2) << "\n";
+    #endif
+    #ifndef ADD
+    std::cout << "\n";
+    #endif
 }
